38ea486aae814208e6dc2b6b0d21cc8c238897fd
ayush <ayushatschool5@gmail.com>
2025-05-15 18:42:37 +0530

    feat: add initial files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.json({ status: "ok", message: "Running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server runnning on port ${PORT}`);
+});
